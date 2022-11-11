2b29b01af34b24d9030ee027253d6c391bab65c0
Rick Segrest <rsegrest77@gmail.com>
2022-11-10 18:58:49 -0600

    Sending username from login screen

--- src/shared/interface/SendMessages.js
+++ src/shared/interface/SendMessages.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
 
 class SendMessages {
-    constructor(socket, mySide) {
+    constructor(socket) {
         this.socket = socket;
-        this.mySide = mySide;
+        // this.mySide = mySide;
     }
     sendUsername = (username) => {
         console.log('sending username : ' + username)
-        this.socket.emit('player_username', {'name': username})
+        this.socket.emit('register_username', {'name': username})
     }
     // joinRoom = () => {
     //     this.socket.emit('join', {'room': 'tictactoe'})
--- src/shared/login/LogInScreen.jsx
+++ src/shared/login/LogInScreen.jsx
@@ -1,16 +1,22 @@
+import { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fontsource/cabin/400.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../chatroom.css';
+import io from 'socket.io-client';
 
 import SendMessages from '../interface/SendMessages';
+import { Form } from 'react-bootstrap';
 
-// const socket = io("http://127.0.0.1:5000/")
+const socket = io("http://127.0.0.1:5000/")
 
   
 function LogInScreen() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const sm = new SendMessages(socket);
   return (
     <>
       <Table
@@ -33,31 +39,51 @@ function LogInScreen() {
           </tr>
           <tr>
             <td
+              className='login-row'
               style={{
                 border: '1px solid lightgray',
                 width: '30rem',
                 height: '5rem',
               }}
             >
-              Username Row
+              Username
+              <Form>
+                <Form.Control
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                    console.log('username is now: ' + username);
+                  }}
+                  type="text"
+                  placeholder="Enter Username"
+                />
+              </Form>
             </td>
           </tr>
           <tr>
             <td
+              className='login-row'
               style={{
                 border: '1px solid lightgray',
                 width: '30rem',
                 height: '5rem',
               }}
             >
-              Password Row
+              Password
+              <Form.Control
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  type="password"
+                  placeholder="Enter Password"
+                />
             </td>
           </tr>
           <tr>
             <td>
               <Button
                 onClick={() => {
-                  SendMessages.sendUsername('tricky');
+                  console.log('about to send username: ', username);
+                  sm.sendUsername(username);
                 }}
               >Register</Button>
             </td>
